251eca46ebb61ba319b5cc38aa2bdc37330377ae
Tanner Rogalsky <the.t.dawg@gmail.com>
2013-05-21 07:52:48 -0400

    begin refactor to use moai props and decks

--- map_entity.lua
+++ map_entity.lua
@@ -11,21 +11,11 @@ function MapEntity:initialize(parent, x, y, width, height, z)
   self.parent = parent
   self.x, self.y = x, y
   self.width, self.height = width or 1, height or 1
-  self.z = z or 1
+
+  self.props = {}
 end
 
 function MapEntity:update(dt)
-end
-
-function MapEntity:render()
-  if self.image then
-  else
-    self.parent:each(function(tile)
-      g.setColor(COLORS.yellow:rgb())
-      local x, y = self.parent:grid_to_world_coords(tile.x, tile.y)
-      g.rectangle("fill", x, y, self.parent.tile_width, self.parent.tile_height)
-    end, self.x, self.y, self.width, self.height)
-  end
 end
 
 function MapEntity:insert_into_grid()
@@ -44,17 +34,6 @@ function MapEntity:move(delta_x, delta_y)
   self:remove_from_grid()
   self.x, self.y = self.x + delta_x, self.y + delta_y
   self:insert_into_grid()
-end
-
-function MapEntity:__lt(other)
-  if self.z < other.z then return true
-  elseif self.z == other.z and self.id < other.id then return true
-  else return false
-  end
-end
-
-function MapEntity:__le(other)
-  return self < other
 end
 
 function MapEntity:quit()
